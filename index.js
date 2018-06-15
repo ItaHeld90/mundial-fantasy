ca8992f848723f7a167b29399e217bf3ea857219
Itamar Held <ita@gmail.com>
2018-06-15 20:03:09 +0300

    cancel xp by price

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const positionScores = require('./data/position-scores.json');
 const _ = require('lodash');
 const { min, max, over } = require('lodash');
 
-const budget = 110;
-const numPlayers = 11;
-const minPrice = 3;
-const maxPrice = 15;
+const budget = 100;
+const numPlayers = 10;
 
 // calculate xp for each player
 const playersWithXp = _(scorers)
@@ -18,23 +16,8 @@ const playersWithXp = _(scorers)
 	}))
 	.value();
 
-const destAveragePrice = Math.floor(budget / numPlayers);
-
-// calculate an estimation of xp per price unit
-const [minXp, maxXp] = over(min, max)(playersWithXp.map(({ xp }) => xp));
-const xpPerPriceUnit = (maxXp - minXp) / (maxPrice - minPrice);
-
-// calculate xp by price for players
-const playersWithXpByPrice = _(playersWithXp)
-	.map(player => ({
-		...player,
-		xpByPrice: calcPlayerXpByPrice(player, xpPerPriceUnit, destAveragePrice)
-	}))
-	.orderBy(({ xpByPrice }) => xpByPrice, 'desc')
-	.value();
-
 // output the results
-printResults(playersWithXpByPrice);
+printResults(playersWithXp);
 
 function calcPlayerXpByPrice(playerWithXp, xpPerPriceUnit, destAveragePrice) {
 	const { xp, Price: price } = playerWithXp;
