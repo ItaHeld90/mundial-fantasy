d7775aab8d52a396765e4bc4ed77b2be7ac0e96b
Itamar Held <ita@gmail.com>
2018-06-16 06:06:40 +0300

    get top teams

--- genetic-algo-runner.js
+++ genetic-algo-runner.js
@@ -1,19 +1,30 @@
 const _ = require('lodash');
-const { times, head } = require('lodash');
+const { times, keyBy, pick } = require('lodash');
+const uuid = require('uuid/v4');
 const { getRandomTeam } = require('./random-team-utils');
 
 function run(topPlayersByPositionAndPrice) {
-    const teams = times(50, () => getRandomTeam(topPlayersByPositionAndPrice));
+    const teams = keyBy(
+        times(50, () => getRandomTeam(topPlayersByPositionAndPrice)),
+        _ => uuid()
+    );
 
+    runGeneticAlgo(teams);
+}
+
+function runGeneticAlgo(teams) {
     const totalXps = _(teams)
-        .map(team => ({
-            ...team,
-            totalXp: getTeamTotalXp(team)
-        }))
-        .orderBy(({ totalXp }) => totalXp, 'desc')
+        .mapValues(getTeamTotalXp)
+        .entries()
+        .orderBy(([, totalXp]) => totalXp, 'desc')
         .value();
-
-    console.log(totalXps);
+    
+    const topTeamIds = _(totalXps)
+        .take(5)
+        .map(([id]) => id)
+        .value();
+    
+    topTeams = pick(teams, topTeamIds);
 }
 
 function getTeamTotalXp(team) {
